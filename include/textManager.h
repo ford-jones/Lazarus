db3ae641ff7f8ccfd5543ef3106c1411c3f4832c
ford-jones <fordnicholasjones@gmail.com>
2024-08-21 12:00:16 +1200

    Update: Begin setting up text manager class

---
+++ include/textManager.h
@@ -0,0 +1,61 @@
+//              .,                                                                                                          .(*      ,*,                
+//                ((.     (.    ,                                                                          */*  ,%##%%%%%%%%%%%%%%%#(*           .      
+//         .//**/#%%%%%%%%%%%%%%#*   .,                                                             ,**   .(%%%%%%#(******,***,/#%%%%%%%%###(/,         
+//             #%%%#*.#%/***,,..,*(%(.    ,,                                                     *     /#%##/*****,,,,,,,,,.,...,,#%,  .#%#.            
+//    .,     *%&#/   %#**,,*..,....,.*#,     ..                                               *     ,%#%#/*,,*,*,,,,,.,.,,.,...,...((     /#(//*/**.    
+//           (%#    *#*...,.,,..........*/      ,                                          .      *#%(#(**,,,,,,,,..,..,..,,........(.     *#(          
+//           *#     *(......,.............(#      ,                                       .     ,((, ##,,,.....,.................. ./       **  .,.     
+//            *     ./........ ...........*#*,                                          ,      ,(,  ./*,,,..,,.................  .. *                   
+//                   /, ........    ... ../(  *.                                              ,*     /,...,,.,,.....   ............**                   
+//                    *... .............  /    ,                                             *,      ,*,,............  ,....     ...                    
+//                     *.   ..... .... ..*                                                  .*        *...................   .  ...                     
+//               *       ... ......... ,.                                                   ,          ... ..........  ...     ..       ,               
+//                ((        .,.,.. ...                                                                   .  . .. .  .  ... .  ..      //                
+//              ,/(#%#*                                                                                     .....  ... ......       .#*                 
+//                 /((##%#(*                                                                                      .......        ,(#(*,                 
+//               (.           .,,,,,                                                                                        .*#%%(                      
+//                                                                                                      .***,.   . .,/##%###(/.  ...,,.      
+/*  LAZARUS ENGINE */
+
+#include <string>
+#include <memory>
+
+#include <../include/fileReader.h>
+
+#ifndef LAZARUS_TEXT_MANAGER_H
+#define LAZARUS_TEXT_MANAGER_H
+
+using std::string;
+using std::unique_ptr;
+
+class TextManager
+{
+    public:
+        TextManager();
+        struct Text 
+        {
+            string body;
+            string font;
+
+            int width;
+            int height;
+        };
+
+        Text loadText(string text, int symbolWidth, int symbolHeight, string fontPath);
+        void drawText(Text text);
+
+        virtual ~TextManager();
+
+    private:
+        void createQuad(int width, int height);
+
+        int symbolWidth;
+        int symbolHeight;
+
+        unique_ptr<FileReader> fontLoader;
+        
+        FileReader::Image symbolSet;
+        Text text;
+};
+
+#endif
---
+++ src/textManager.cpp
@@ -0,0 +1,51 @@
+//              .,                                                                                                          .(*      ,*,                
+//                ((.     (.    ,                                                                          */*  ,%##%%%%%%%%%%%%%%%#(*           .      
+//         .//**/#%%%%%%%%%%%%%%#*   .,                                                             ,**   .(%%%%%%#(******,***,/#%%%%%%%%###(/,         
+//             #%%%#*.#%/***,,..,*(%(.    ,,                                                     *     /#%##/*****,,,,,,,,,.,...,,#%,  .#%#.            
+//    .,     *%&#/   %#**,,*..,....,.*#,     ..                                               *     ,%#%#/*,,*,*,,,,,.,.,,.,...,...((     /#(//*/**.    
+//           (%#    *#*...,.,,..........*/      ,                                          .      *#%(#(**,,,,,,,,..,..,..,,........(.     *#(          
+//           *#     *(......,.............(#      ,                                       .     ,((, ##,,,.....,.................. ./       **  .,.     
+//            *     ./........ ...........*#*,                                          ,      ,(,  ./*,,,..,,.................  .. *                   
+//                   /, ........    ... ../(  *.                                              ,*     /,...,,.,,.....   ............**                   
+//                    *... .............  /    ,                                             *,      ,*,,............  ,....     ...                    
+//                     *.   ..... .... ..*                                                  .*        *...................   .  ...                     
+//               *       ... ......... ,.                                                   ,          ... ..........  ...     ..       ,               
+//                ((        .,.,.. ...                                                                   .  . .. .  .  ... .  ..      //                
+//              ,/(#%#*                                                                                     .....  ... ......       .#*                 
+//                 /((##%#(*                                                                                      .......        ,(#(*,                 
+//               (.           .,,,,,                                                                                        .*#%%(                      
+//                                                                                                      .***,.   . .,/##%###(/.  ...,,.      
+/*  LAZARUS ENGINE */
+
+#include "../include/textManager.h"
+
+TextManager::TextManager()
+{
+    this->symbolHeight = 0;
+    this->symbolWidth = 0;
+
+    this->fontLoader = nullptr;
+};
+
+TextManager::Text TextManager::loadText(string text, int symbolWidth, int symbolHeight, string fontPath)
+{
+    fontLoader = std::make_unique<FileReader>();
+    symbolSet = fontLoader->readFromImage(fontPath);
+
+    this->createQuad(symbolWidth, symbolHeight);
+}
+
+void TextManager::drawText(TextManager::Text text)
+{
+
+};
+
+void TextManager::createQuad(int width, int height)
+{
+
+}
+
+TextManager::~TextManager()
+{
+
+};
