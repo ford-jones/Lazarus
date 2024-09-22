276da243df6baa228815bdcf8d0ea876e448afe1
ford-jones <fordnicholasjones@gmail.com>
2024-09-22 12:38:27 +1200

    Feat: Define required resizing headers

--- src/imageLoader.cpp
+++ src/imageLoader.cpp
@@ -2,3 +2,8 @@
 #define STB_IMAGE_IMPLEMENTATION
 	#include <stb_image.h>
 #endif
+
+#ifndef STB_IMAGE_RESIZE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+	#include <stb_image_resize.h>
+#endif
