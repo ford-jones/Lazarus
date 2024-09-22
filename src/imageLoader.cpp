5737589dab8d9065b6951bf2435c43d5c896ae72
ford-jones <fordnicholasjones@gmail.com>
2024-09-22 16:53:11 +1200

    Feat: Correctly implement stb headers

--- src/imageLoader.cpp
+++ src/imageLoader.cpp
@@ -2,8 +2,3 @@
 #define STB_IMAGE_IMPLEMENTATION
 	#include <stb_image.h>
 #endif
-
-#ifndef STB_IMAGE_RESIZE_IMPLEMENTATION
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-	#include <stb_image_resize.h>
-#endif
